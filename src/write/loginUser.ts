2b353031ea83296160b43c2d55a64b27153c8392
doramatadora <dora.militaru@ft.com>
2018-03-27 13:28:42 +0100

    add invalid options test to loginUser  🐿 v2.8.0

--- src/write/loginUser.ts
+++ src/write/loginUser.ts
@@ -4,6 +4,5 @@ import { validateOptions } from '../utils/validate';
 
 export const loginUser = (opts: LoginUserOptions): Promise<any> => {
 	validateOptions(opts, null, ['email', 'password', 'apiHost', 'apiKey']);
-	const { email, password, apiHost, apiKey } = opts;
-	return userLoginApi({ email, password, apiHost, apiKey });
+	return userLoginApi(opts);
 };
