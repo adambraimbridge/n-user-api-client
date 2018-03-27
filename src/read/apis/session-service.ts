509a1362364c2a7b622f51fe88ddd53db25b82e4
doramatadora <dora.militaru@ft.com>
2018-03-27 13:35:21 +0100

    same for session service  🐿 v2.8.0

--- src/read/apis/session-service.ts
+++ src/read/apis/session-service.ts
@@ -2,41 +2,32 @@ import 'isomorphic-fetch';
 import { errorTypes, ErrorWithData } from '../../utils/error';
 import { isSessionStale } from '../transforms/session-freshness';
 
-export const getSessionData = ({ session, apiHost, apiKey }) => {
-	return new Promise(async (resolve, reject) => {
-		const url = `${apiHost}/sessions/s/${session}`;
-		const errorMsg = 'Could not get session data';
-		const options = {
-			timeout: 10000,
-			headers: {
-				'Content-Type': 'application/json',
-				'X-Api-Key': apiKey
-			},
-			method: 'GET'
+export const getSessionData = async ({ session, apiHost, apiKey }) => {
+	const url = `${apiHost}/sessions/s/${session}`;
+	const errorMsg = 'Could not get session data';
+	const options = {
+		timeout: 10000,
+		headers: {
+			'Content-Type': 'application/json',
+			'X-Api-Key': apiKey
+		},
+		method: 'GET'
+	};
+	const response = await fetch(url, options);
+	if (response.ok) {
+		const body = await response.json();
+		if (!body.uuid)
+			throw new ErrorWithData('Valid user ID not returned', {
+				type: errorTypes.API
+			});
+		return {
+			userId: body.uuid,
+			isSessionStale: isSessionStale(body.creationTime)
 		};
-		try {
-			const response = await fetch(url, options);
-			if (response.ok) {
-				const body = await response.json();
-				if (!body.uuid)
-					throw new ErrorWithData('Valid user ID not returned', {
-						type: errorTypes.API
-					});
-				const sessionData = {
-					userId: body.uuid,
-					isSessionStale: isSessionStale(body.creationTime)
-				};
-				return resolve(sessionData);
-			}
-			reject(
-				new ErrorWithData(errorMsg, {
-					url,
-					statusCode: response.status,
-					type: response.status === 403 ? errorTypes.API_KEY : errorTypes.API
-				})
-			);
-		} catch (err) {
-			reject(err);
-		}
+	}
+	throw new ErrorWithData(errorMsg, {
+		url,
+		statusCode: response.status,
+		type: response.status === 403 ? errorTypes.API_KEY : errorTypes.API
 	});
 };
