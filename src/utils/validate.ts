ad2b823aaeaea801f98891ee66ed01243db4dc2b
doramatadora <dora.militaru@ft.com>
2018-03-27 13:16:25 +0100

    equivalent pattern on loginUser  🐿 v2.8.0

--- src/utils/validate.ts
+++ src/utils/validate.ts
@@ -1,13 +1,17 @@
 export const validateOptions = (opts, dataOption, stringOpts = []) => {
-	if (!opts)
-			throw new Error('Options not supplied');
+	if (!opts) {
+		throw new Error('Options not supplied');
+	}
 	let invalidOptions = [];
 	stringOpts.forEach(stringOpt => {
-			if (typeof opts[stringOpt] !== 'string')
-					invalidOptions.push(stringOpt);
+		if (typeof opts[stringOpt] !== 'string') {
+			invalidOptions.push(stringOpt);
+		}
 	});
-	if (dataOption && typeof opts[dataOption] !== 'object')
-			invalidOptions.push(dataOption);
-	if (invalidOptions.length)
-			throw new Error(`Invalid option(s): ${invalidOptions.join(', ')}`);
+	if (dataOption && typeof opts[dataOption] !== 'object') {
+		invalidOptions.push(dataOption);
+	}
+	if (invalidOptions.length) {
+		throw new Error(`Invalid option(s): ${invalidOptions.join(', ')}`);
+	}
 };
--- src/write/loginUser.ts
+++ src/write/loginUser.ts
@@ -1,15 +1,9 @@
-import {userLoginApi} from './apis/user-login';
-import {LoginUserOptions} from '../types';
-import {validateOptions} from '../utils/validate';
+import { userLoginApi } from './apis/user-login';
+import { LoginUserOptions } from '../types';
+import { validateOptions } from '../utils/validate';
 
-export const loginUser = async (opts: LoginUserOptions): Promise<any> => {
-	return new Promise(async (resolve, reject) => {
-		try {
-			validateOptions(opts, null, ['email', 'password', 'apiHost', 'apiKey'])
-			const {email, password, apiHost, apiKey} = opts;
-			resolve(await userLoginApi({email, password, apiHost, apiKey}));
-		} catch (error) {
-			reject(error);
-		}
-	});
+export const loginUser = (opts: LoginUserOptions): Promise<any> => {
+	validateOptions(opts, null, ['email', 'password', 'apiHost', 'apiKey']);
+	const { email, password, apiHost, apiKey } = opts;
+	return userLoginApi({ email, password, apiHost, apiKey });
 };
